12148d8b9082685d1cbc029acb0336d444e67066
Ozodbek <tursunovozodbek505@gmail.com>
2023-07-13 17:24:18 +0500

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import adviceGenerator from "../assets/projects/adviceGenerator.jpg";
+import coffeeWebsite from "../assets/projects/coffeeWebsite.jpg";
+import movieApp from "../assets/projects/movieApp.jpg";
+import netflixClone from "../assets/projects/NetflixClone.jpg";
+import showMobileApp from "../assets/projects/showMobileApp.jpg";
+import weatherWebsite from "../assets/projects/weatherWebsite.jpg";
+
+export const data = [
+  {
+    id: 1,
+    name: "React JS Application",
+    image: adviceGenerator,
+    github: "https://github.com/tursunovozodbek/advice_app",
+    live: "https://6499a9ddb472f80090a3d31b--cozy-tarsier-2f6aad.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "HTML + CSS",
+    image: coffeeWebsite,
+    github: "https://github.com/tursunovozodbek/StoreofCoffe",
+    live: "https://64a2806f57067b5d5593f179--symphonious-peony-459817.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "HTML + CSS",
+    image: movieApp,
+    github: "https://github.com/tursunovozodbek/MovieWebsite",
+    live: "https://64a280c9f8f8e65f5aff4618--precious-marigold-1e5a0d.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "HTML + CSS",
+    image: netflixClone,
+    github: "https://github.com/tursunovozodbek/NetflixClone",
+    live: "https://64a28165f8f8e65f5aff4947--superb-lily-010e5d.netlify.app/",
+  },
+  {
+    id: 5,
+    name: "HTML + CSS",
+    image: showMobileApp,
+    github: "https://github.com/tursunovozodbek/ShowMobileApp",
+    live: "https://64a281e5c002915f078b5ace--spiffy-selkie-84c804.netlify.app/",
+  },
+  {
+    id: 6,
+    name: "React JS Application",
+    image: weatherWebsite,
+    github: "https://github.com/tursunovozodbek/Weather_App",
+    live: "https://6499a62c9ce0ed0086846e72--sparkling-sherbet-d7cd0e.netlify.app/",
+  },
+];
